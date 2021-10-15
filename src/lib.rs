a0317076812bc07bfda71026095daffc98ec7430
Arp-G <arpanghoshal3@gmail.com>
2021-10-15 13:44:06 +0530

    finish basic tutorial

--- src/lib.rs
+++ src/lib.rs
@@ -20,11 +20,33 @@ pub fn greet(name: &str) {
 }
 
 extern crate web_sys;
+use web_sys::console;
 
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
     ( $( $t:tt )* ) => {
-        web_sys::console::log_1(&format!( $( $t )* ).into());
+        console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+// Implement a timer struct to check how much time a method took
+// W eare using the `console.time` and `console.timeEnd` from javascript here.
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+// We implement the `Drop` trait here so the `drop` method will be called whenever the `timer` instance
+// goes out of scope this will trigger the js function `console.timeEnd` to print the time taken
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
     }
 }
 
@@ -57,7 +79,15 @@ pub struct Universe {
     cells: Vec<Cell>,
 }
 
-// This `impl` block is annotated with #[wasm_bindgen] so that it can be called by JavaScript. 
+fn get_random_cell() -> Cell {
+    if js_sys::Math::random() < 0.5 {
+        Cell::Alive
+    } else {
+        Cell::Dead
+    }
+}
+
+// This `impl` block is annotated with #[wasm_bindgen] so that it can be called by JavaScript.
 #[wasm_bindgen]
 impl Universe {
     // Get 1D array index for a given 2D array index
@@ -122,8 +152,32 @@ impl Universe {
         self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
     }
 
+    /// Sets the universe to a random state
+    pub fn randomize(&mut self) {
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                self.cells[idx] = get_random_cell();
+            }
+        }
+    }
+
+    /// Resets the universe to all dead cells
+    pub fn reset(&mut self) {
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                self.cells[idx] = Cell::Dead;
+            }
+        }
+    }
+
     // Compute the next generation of the universe
     pub fn tick(&mut self) {
+
+        // To track execution time for every tick
+        let _timer = Timer::new("Universe::tick");
+
         let mut next = self.cells.clone(); // Next generation
 
         for row in 0..self.height {
@@ -176,15 +230,7 @@ impl Universe {
         let width = 64;
         let height = 64;
 
-        let cells = (0..width * height)
-            .map(|_| {
-                if js_sys::Math::random() < 0.5 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
+        let cells = (0..width * height).map(|_| get_random_cell()).collect();
 
         log!("Init Universe from wasm");
 
